1b4510658fd690b477aa001f5b00af634ec3043d
unknown <sv73107535@idat.edu.pe>
2021-09-08 18:42:35 -0500

    first master

---
+++ index.js
@@ -0,0 +1,10 @@
+//--------------------------------------------------//
+for (let i = 0; i <= 20; i++) {
+    if(i % 2 === 0) console.log(`${i} es par`);
+    if(i % 2 === 1) console.log(`${i} es impar`);
+}
+//--------------------------------------------------//
+const multiplier = 9;
+for (let i = 0; i <= 10; i++) {
+    console.log(`${i} * 9 = ${i*multiplier}`);
+}
---
+++ tabla.js
@@ -0,0 +1,6 @@
+for (let i = 0; i <=10; i++){
+    console.log(`Tabla del ${i}`);
+    for (let j = 0; j <=10; j++) {
+        console.log(`${i} * ${j} = ${i*j}`);
+    }
+}
